9a1585571012c114b75ca5bfbe1d0dab8a08ecf8
nikhilakki <nikhilakki@users.noreply.github.com>
2017-06-17 11:27:08 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
 def solution(keys, values):
     '''Enter Code Here'''
+    new_dic = {}
+    for i,v in enumerate(values):
+        i +=1
+        d = dict({i : v})
+        new_dic.update(d)
+    return new_dic
+
+
+
+keys = [1, 2, 3]
+vals = [10, 20, 30]
+print(solution(keys, vals))
