077f1da033dbf5a3099b61b6aa210125ebb90d80
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-17 01:33:17 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,7 @@
 def solution(keys, values):
-    '''Enter Code Here'''
+    '''TO get the key value pair thru two different list as form of dictionary'''
+    new_dic = {k: v for k,v in zip(keys,values)}
+    print new_dic
+    return new_dic
+
+solution([1, 2, 3], [10, 20, 30])
