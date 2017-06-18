00093e5133f5f135e43281896b0c4c91bdf0a4a8
nixen420 <nikhil.singh4008@gmail.com>
2017-06-18 09:58:09 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
-def solution(keys, values):
-    '''Enter Code Here'''
+def solution(x,y):
+    dic = {}
+    if len(x) == len(y):
+        z = 0
+        for i in x:
+            dic[i] = y[z]
+            z = z+1
+    return dic
+solution([1, 2, 3], [10, 20, 30])
