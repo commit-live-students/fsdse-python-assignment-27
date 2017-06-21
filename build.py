11058c546b376fb25895cb0da9a507880a9ce68f
ramkrishan-sahu <ram04190@gmail.com>
2017-06-21 13:45:35 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
-def solution(keys, values):
-    '''Enter Code Here'''
+# Map two lists into a dictionary
+
+a = [1, 2, 3]
+b = [10, 20, 30]
+
+def solution(a,b):
+    dic = {}
+    for i in range(len(a)):
+        dic[a[i]] = b[i]
+    return dic
+
+solution(a,b)
+
+#DOne, Not Posted =================================================================================
