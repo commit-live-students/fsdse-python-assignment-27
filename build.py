ba13036ed1a7a659e84c2dd127bd3d703d97983a
rishabh-github <rishabh-github@users.noreply.github.com>
2017-06-24 18:53:26 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
+import itertools
+
 def solution(keys, values):
-    '''Enter Code Here'''
+    newdict = dict(itertools.izip(keys,values))
+    return newdict
+
+
+print solution([1, 2, 3], [10, 20, 30])
