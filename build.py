38e2f911729a6a97864fec6b5114d7d3ce8cc12f
rohanhg91 <rohanhgupta91@gmail.com>
2017-08-14 16:10:02 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
 def solution(keys, values):
     '''Enter Code Here'''
+    dic = {}
+    for i in range (0,len(keys)):
+        dic[keys[i]] = values[i]
+    return dic
+
+
+'''
+a = solution([1,2,4,8],[12,21,31,11])
+print a
+'''
