dbe9cb5247913e7269ebef06d09567f963effbaf
darshind <doshi.darshin@gmail.com>
2017-08-16 14:52:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
-def solution(keys, values):
-    '''Enter Code Here'''
+def solution(l1,l2):
+    k = zip(l1,l2)
+    d = dict()
+    for (x,y) in k:
+
+        if x in d:
+            d[x] = d[x] + y
+        else:
+            d[x] = y
+
+    return d
+print solution([1,2,3],[10,20,30])
